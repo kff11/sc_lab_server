811e1a130fe47523634ac1b8194c13b570bca889
kff11 <55500009+kff11@users.noreply.github.com>
2020-05-03 23:28:41 +0900

    Seat getState Modify

--- server/route.js
+++ server/route.js
@@ -5,7 +5,7 @@ const controller = require('./controller');
 router.post('/user/login', controller.user.login);
 router.post('/user/signUp', controller.user.signUp);
 router.post('/seat/reserve', controller.seat.reserve);
-router.get('/seat/getState', controller.seat.getState)
+router.post('/seat/getState', controller.seat.getState)
 
 
 module.exports = router;
